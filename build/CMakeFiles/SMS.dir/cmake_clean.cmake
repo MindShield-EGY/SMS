f4594568a40e3b554a84b2039b16f549feb5a49f
Masrkai <ahmedelsayedallamsquizeesfx@gmail.com>
2024-05-17 21:48:13 +0300

    @Masrkai

---
+++ build/CMakeFiles/SMS.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SMS.dir/SMS.cpp.o"
+  "CMakeFiles/SMS.dir/SMS.cpp.o.d"
+  "SMS"
+  "SMS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SMS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/SMS.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SMS.
+# This may be replaced when dependencies are built.
